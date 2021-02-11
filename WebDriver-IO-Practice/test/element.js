bd1d26e0a8c72c82cd2afa37b6564d8a9314cd4d
André Tavares <andreftavares8@gmail.com>
2021-02-11 02:27:25 +0000

    second practical project
    second project with webdriverIO

---
+++ WebDriver-IO-Practice/test/element.js
@@ -0,0 +1,19 @@
+describe("interaction with web element", function(){
+
+  it("enter value in a field", function(){
+    browser.url('/');
+    const search = $('#twotabsearchtextbox');
+    search.setValue('Apple Mac Book');    
+  });
+
+  it("get text of field", function(){
+    const span = $('#glow-ingress-line2');
+    let text = span.getText();
+    console.log(text); 
+  });
+  
+  it("click on a field", function(){
+    const btnSearch = $('#nav-search-submit-button');
+    btnSearch.click()
+  });
+});
---
+++ WebDriver-IO-Practice/test/header.js
@@ -0,0 +1,10 @@
+describe("interaction with web element", function() {
+
+  it("enter header value", function() {
+    browser.url('https://www.freshworks.com');
+    const header = $('h1');
+    let text = header.getText();
+    console.log(text);   
+  });
+
+});
