b9e7d176b0c7d806ce70688b2eb9957a0e5f8b85
André Tavares <andreftavares8@gmail.com>
2021-02-11 03:27:46 +0000

    Update element.js

--- WebDriver-IO-Practice/test/element.js
+++ WebDriver-IO-Practice/test/element.js
@@ -3,17 +3,15 @@ describe("interaction with web element", function(){
   it("enter value in a field", function(){
     browser.url('/');
     const search = $('#twotabsearchtextbox');
-    search.setValue('Apple Mac Book');    
+    search.setValue('Apple Mac Book');
   });
-
   it("get text of field", function(){
     const span = $('#glow-ingress-line2');
     let text = span.getText();
-    console.log(text); 
+    console.log(text);
   });
-  
   it("click on a field", function(){
     const btnSearch = $('#nav-search-submit-button');
-    btnSearch.click()
+    btnSearch.click();
   });
 });
